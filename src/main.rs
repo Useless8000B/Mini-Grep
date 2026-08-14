b0b3f8ee53cebdfdbb4852c1f4deff19bb5d4ca7
Useless8000B <adrielluiz73@gmail.com>
2026-08-14 17:23:48 -0300

    Implement logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use crate::config::Config;
+use mini_grep::search;
+use std::{env, fs};
+mod config;
+
+fn run(config: Config) -> Result<(), Box<dyn std::error::Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    for line in search(config.query, &contents) {
+        println!("{line}");
+    }
+
+    Ok(())
+}
+
 fn main() {
-    println!("Hi mom!");
+    let args: Vec<String> = env::args().collect();
+    let config = match Config::new(&args) {
+        Ok(b) => b,
+        Err(e) => {
+            println!("Error creating config: {e}");
+            std::process::exit(1)
+        }
+    };
+
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        std::process::exit(1);
+    };
 }
